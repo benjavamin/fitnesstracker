4ee75864070ae9769d5fea103875547f011c449e
Benjamin <bajic.benjamin98@gmail.com>
2019-09-21 14:37:39 +0100

    routing, auth guard, exercise model

--- Fitness-Tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ Fitness-Tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, OnDestroy} from '@angular/core';
+import {AuthService} from '../../auth/auth.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
 
   @Output() closeSidenav = new EventEmitter<void>();
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  isAuth = false;
+  authSubscription: Subscription;
 
   ngOnInit() {
+    this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
   onClose() {
     this.closeSidenav.emit();
+  }
+
+  onLogout() {
+    this.onClose();
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 }
